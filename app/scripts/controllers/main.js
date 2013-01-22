54fec7f52b6b544d950dda61f29d97231064716a
shai <shai@fashiontraffic.com>
2013-01-22 22:46:53 +0200

    step4 initial state

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,21 +1,14 @@
 'use strict';
 
-angularDemoApp.controller('MainCtrl', function($scope, userService) {
+angularDemoApp.controller('MainCtrl', function($scope) {
 
-  // $scope.users = [
-  //   {name: "Bar", huggable: true, image: "images/bar.png"},
-  //   {name: "Tzahi", huggable: false, image: "images/tzahi.jpg"},
-  //   {name: "Natalie", huggable: true, image: "images/natalie.jpeg"},
-  //   {name: "Fuad", huggable: true, image: "images/fuad.jpg"},
-  //   {name: "Angie", huggable: false, image: "images/angie.jpeg"},
-  //   {name: "Berlad", huggable: true, image: "images/berlad.jpg"}
-  // ];
+  $scope.users = [
+    {name: "Bar", huggable: true, image: "images/bar.png"},
+    {name: "Tzahi", huggable: false, image: "images/tzahi.jpg"},
+    {name: "Natalie", huggable: true, image: "images/natalie.jpeg"},
+    {name: "Fuad", huggable: true, image: "images/fuad.jpg"},
+    {name: "Angie", huggable: false, image: "images/angie.jpeg"},
+    {name: "Berlad", huggable: true, image: "images/berlad.jpg"}
+  ];
 
-  $scope.users = userService.users;
-
-  $scope.showRules = false;
-
-  $scope.toggleShowRules = function(){
-  	$scope.showRules = !$scope.showRules;
-  }
 });

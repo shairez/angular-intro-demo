abfd53676dadc2f3a14b33b2b7f29ff0179b0497
shai <shai@fashiontraffic.com>
2013-01-22 22:17:23 +0200

    step2 - initial state

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,20 +2,26 @@
 
 angularDemoApp.controller('MainCtrl', function($scope, userService) {
 
-  // $scope.users = [
-  //   {name: "Bar", huggable: true, image: "images/bar.png"},
-  //   {name: "Tzahi", huggable: false, image: "images/tzahi.jpg"},
-  //   {name: "Natalie", huggable: true, image: "images/natalie.jpeg"},
-  //   {name: "Fuad", huggable: true, image: "images/fuad.jpg"},
-  //   {name: "Angie", huggable: false, image: "images/angie.jpeg"},
-  //   {name: "Berlad", huggable: true, image: "images/berlad.jpg"}
-  // ];
-
-  $scope.users = userService.users;
+  $scope.users = [
+    {name: "Bar", huggable: true, image: "images/bar.png"},
+    {name: "Tzahi", huggable: false, image: "images/tzahi.jpg"},
+    {name: "Natalie", huggable: true, image: "images/natalie.jpeg"},
+    {name: "Fuad", huggable: true, image: "images/fuad.jpg"},
+    {name: "Angie", huggable: false, image: "images/angie.jpeg"},
+    {name: "Berlad", huggable: true, image: "images/berlad.jpg"}
+  ];
 
   $scope.showRules = false;
 
   $scope.toggleShowRules = function(){
   	$scope.showRules = !$scope.showRules;
   }
+
+  	$scope.huggableClass = function(user){
+        return (user.huggable) ? "huggable" : "not-huggable";
+    }
+
+    $scope.huggableStatus = function(user){
+       return (user.huggable) ? "Sure" : "Not right now";
+    }
 });
